c8c827b7a0fd8d24a17154ba674571e28af28247
Fabio Francisco <fabinhofrancisco@gmail.com>
2021-11-15 23:25:50 -0300

    feat: create route para upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+
+/**
+ * Database setup
+ */
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+});
+
 require('express-async-errors');
 
 const routes = require('./routes');
@@ -8,9 +22,15 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 app.use(cors);
+
+app.use(
+  'files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')),
+);
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3000, () => console.log('API started at http://localhost:3000'));
+app.listen(process.env.APP_PORT, () => console.log(`API started at ${process.env.APP_URL}:${process.env.APP_PORT}`));
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,40 @@
 const { Router } = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-const router = Router();
+const multerMiddlewere = require('./middlewares/multer');
 
-router.get('/', (request, response) => {
-  response.status(200).json({'ok': 'Rota Raiz'})
-} );
+const Post = require('./models/Post');
 
+const routes = Router();
 
-module.exports = router;
+routes.get('/posts', async (request, response) => {
+  const posts = await Post.find();
+  return response.json(posts);
+});
+
+routes.post('/posts', multerMiddlewere, multer(multerConfig).single('file'), async (request, response) => {
+  // console.log(request.file);
+
+  const {
+    originalname: name, size, key, location: url = '',
+  } = request.file;
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url,
+  });
+
+  response.status(200).json(post);
+});
+
+routes.delete('/posts/:id', async (request, response) => {
+  const post = await Post.findById(request.params.id);
+
+  await post.remove();
+
+  return response.send();
+});
+module.exports = routes;
